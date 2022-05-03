263d51efcf779aabb120e1db1456d19694933e55
Rafkat Akhmerov <92851653+Rafkat9@users.noreply.github.com>
2022-05-03 20:28:41 +0500

    Delete core directory

--- core/routes.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-return [
-    '' => ['IndexController' , 'indexMethod'],
-    'auth' => ['AuthController' , 'authMethod'],
-    'reg' => ['RegController' , 'RegMethod'],
-    'notes' => ['NotesController' , 'NotesMethod'],
-    'home' => ['HomeController' , 'HomeMethod']
-];
